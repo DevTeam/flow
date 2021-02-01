0e1ce2a888dbd52ab319ed0271419a06b7ee8f4e
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-02-01 15:05:29 +0300

    Add core lib

---
+++ Flow.Core/Activities.cs
@@ -0,0 +1,42 @@
+﻿namespace Flow.Core
+{
+    using System;
+    using System.Activities;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+#if !NET48
+    using System.Activities.XamlIntegration;
+#endif
+
+    public static class Activities
+    {
+        public static IDictionary<string, object> Run(string activityName)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+            var activities =
+                from type in assembly.DefinedTypes
+                where type.Name.Equals(activityName, StringComparison.CurrentCultureIgnoreCase)
+                where !type.IsAbstract
+                where typeof(Activity).IsAssignableFrom(type)
+                where type.DeclaredConstructors.Any(ctor => ctor.IsPublic && !ctor.GetParameters().Any())
+                select (Activity)Activator.CreateInstance(type);
+
+#if !NET48
+            activities = activities.Concat(
+                from resourceName in assembly.GetManifestResourceNames()
+                where resourceName.EndsWith($"{activityName}.xaml", StringComparison.InvariantCultureIgnoreCase)
+                let stream = assembly.GetManifestResourceStream(resourceName)
+                select ActivityXamlServices.Load(stream));
+#endif
+
+            var activity = activities.FirstOrDefault();
+            if (activity == null)
+            {
+                throw new ArgumentException(nameof(activityName), $"Cannot find \"{activityName}\".");
+            }
+
+            return WorkflowInvoker.Invoke(activity);
+        }
+    }
+}
--- flow/Program.cs
+++ flow/Program.cs
@@ -1,24 +1,10 @@
-﻿#if NET48
-using System.Activities;
-#else
-using System.Activities.XamlIntegration;
-using System.Linq;
-using System.Reflection;
-#endif
-
-namespace flow
+﻿namespace flow
 {
+    using Flow.Core;
+
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-#if NET48
-            WorkflowInvoker.Invoke(new Build());
-#else
-            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(Assembly.GetExecutingAssembly().GetManifestResourceNames().Single(str => str.EndsWith("Build.xaml")));
-            var helloWorldActivity = ActivityXamlServices.Load(stream);
-            System.Activities.WorkflowInvoker.Invoke(helloWorldActivity);
-#endif
-        }
+        public static void Main(string[] args) =>
+            Activities.Run(args.Length > 0 ? args[0] : "Default");
     }
 }
