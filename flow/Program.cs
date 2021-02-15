2543e1ced9238624fe16216b03c1d56214b128f3
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-02-15 15:25:10 +0300

    Add samples

--- flow/Program.cs
+++ flow/Program.cs
@@ -5,8 +5,8 @@ namespace Flow
 
     public static class Program
     {
-        public static void Main() =>
-            new Default().Run(TimeSpan.MaxValue);
+        public static void Main(string[] args) =>
+            Flows.Run(args.Length > 0 ? args[0] : "Default", TimeSpan.MaxValue);
     }
 }
 #endif
