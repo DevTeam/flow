2577085c750af51befb12c013c796162efd2c544
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-03-05 20:27:47 +0300

    Add log for MSBuildResponseFile

--- flow/Core/MSBuildResponseFile.cs
+++ flow/Core/MSBuildResponseFile.cs
@@ -1,12 +1,15 @@
 ﻿namespace Flow.Core
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using IoC;
 
     // ReSharper disable once InconsistentNaming
     // ReSharper disable once ClassNeverInstantiated.Global
     internal class MSBuildResponseFile : IResponseFile
     {
+        private readonly ILog<MSBuildResponseFile> _log;
         private readonly Verbosity _verbosity;
         [NotNull] private readonly ITeamCitySettings _teamCitySettings;
         [NotNull] private readonly IFileSystem _fileSystem;
@@ -15,6 +18,7 @@
         private readonly Path _rspPath;
 
         public MSBuildResponseFile(
+            [NotNull] ILog<MSBuildResponseFile> log,
             Verbosity verbosity,
             [Tag(Tags.TempFile)] Path tempFilePath,
             [NotNull] ITeamCitySettings teamCitySettings,
@@ -22,6 +26,7 @@
             [NotNull] IConverter<MSBuildParameter, string> paramConverter,
             [NotNull] Func<IPathNormalizer> pathNormalizer)
         {
+            _log = log ?? throw new ArgumentNullException(nameof(log));
             _verbosity = verbosity;
             _teamCitySettings = teamCitySettings ?? throw new ArgumentNullException(nameof(teamCitySettings));
             _fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
@@ -33,15 +38,26 @@
         public Path Create()
         {
             var pathNormalizer = _pathNormalizer();
-            _fileSystem.WriteLines(_rspPath, new[]
+            var args = new[]
             {
                 "/noconsolelogger",
                 $"/l:TeamCity.MSBuild.Logger.TeamCityMSBuildLogger,{pathNormalizer.Normalize(_teamCitySettings.MSBuildLogger).Value};TeamCity;verbosity={_verbosity}",
                 "/p:VSTestLogger=logger://teamcity",
                 _paramConverter.Convert(new MSBuildParameter("VSTestTestAdapterPath", $".;{pathNormalizer.Normalize(_teamCitySettings.VSTestLogger).Value}"))
+            };
+
+            _fileSystem.WriteLines(_rspPath, args);
+
+            _log.Trace(() =>
+            {
+                var text = new List<Text> {_rspPath.Value, Text.NewLine };
+                text.AddRange(args.Select(i => new Text(i)).Join(Text.NewLine));
+                return text.ToArray();
             });
 
             return pathNormalizer.Normalize(_rspPath);
         }
+
+
     }
 }
---
+++ flow/Core/TextExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Flow.Core
+{
+    using System.Collections.Generic;
+
+    internal static class TextExtensions
+    {
+        public static IEnumerable<Text> Join(this IEnumerable<Text> text, Text separator)
+        {
+            var first = true;
+            foreach (var item in text)
+            {
+                if (!first)
+                {
+                    yield return separator;
+                }
+                else
+                {
+                    first = false;
+                }
+
+                yield return item;
+            }
+        }
+    }
+}
